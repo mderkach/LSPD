4a728b06823793fdb985c4ac76efe63228d97fd0
mderkach <derkach94@gmail.com>
2019-10-23 12:55:52 +0300

    bump

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,10 @@ module.exports = {
   devServer: {
     disableHostCheck: true,
   },
+  publicPath: process.env.NODE_ENV === 'production' ? '.' : '/',
+  productionSourceMap: true,
+  crossorigin: 'anonymous',
+  css: {
+    sourceMap: true,
+  },
 }
